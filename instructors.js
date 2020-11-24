16dc426eac792df1b832c0f6084adc21d5194341
Vinicius Pereira <viniciusnspp@gmail.com>
2020-11-23 22:19:24 -0300

    calculation Birth and connection backend with frontend

--- instructors.js
+++ instructors.js
@@ -1,5 +1,28 @@
 const fs = require('fs')
 const data = require("./data.json")
+const { age } = require('./util')
+
+exports.show = function(req, res) {
+
+    const { id } = req.params
+
+    const foundInstructor = data.instructors.find(function(instructor){
+        return instructor.id == id
+    })
+
+    if (!foundInstructor) return res.send("Instrutor não encontrado")
+
+    const instructor = {
+        ...foundInstructor,
+        age: age(foundInstructor.birth),
+        services: foundInstructor.services.split(","),
+        created_at: "",
+    }
+
+
+
+    return res.render("instructors/show", { instructor })
+}
 //post
 exports.post = function(req, res) {
 
@@ -10,12 +33,24 @@ exports.post = function(req, res) {
                 return res.send('Por favor, preencha todos os campos!')
             }
         }
+        let {avatar_url, birth, name, services, gender} = req.body
 
-        req.body.birth = Date.parse(req.body.birth)
-        req.body.created_at = Date.now()
+        birth = Date.parse(req.body.birth)
+        const created_at = Date.now()
+        const id = Number(data.instructors.length + 1)
+
         
         
-        data.instructors.push(req.body)
+        data.instructors.push({
+            id,
+            avatar_url,
+            name,
+            birth,
+            gender,
+            services,
+            created_at
+
+        })
 
         fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
             if(err) return res.send("Write file error")
